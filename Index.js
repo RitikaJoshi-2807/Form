fa07eaf47722e216110dd9be509f05017f119711
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-05-07 22:44:33 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,9 @@
+npm ERR! syscall open
+npm ERR! path C:\Users\hp\OneDrive\Desktop\New folder/package.json
+npm ERR! errno -4058
+npm ERR! enoent ENOENT: no such file or directory, open 'C:\Users\hp\OneDrive\Desktop\New folder\package.json'
+npm ERR! enoent This is related to npm not being able to find a file.
+npm ERR! enoent 
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\hp\AppData\Local\npm-cache\_logs\2022-05-07T17_10_51_674Z-debug-0.log
