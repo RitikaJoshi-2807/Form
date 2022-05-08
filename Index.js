2536eaa47d5856e8eb7b2fdeaa541090f1b533a6
Ritika Joshi <99448254+RitikaJoshi-2807@users.noreply.github.com>
2022-05-08 15:21:49 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,9 +1,10 @@
-npm ERR! syscall open
-npm ERR! path C:\Users\hp\OneDrive\Desktop\New folder/package.json
-npm ERR! errno -4058
-npm ERR! enoent ENOENT: no such file or directory, open 'C:\Users\hp\OneDrive\Desktop\New folder\package.json'
-npm ERR! enoent This is related to npm not being able to find a file.
-npm ERR! enoent 
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-npm ERR! A complete log of this run can be found in:
-npm ERR!     C:\Users\hp\AppData\Local\npm-cache\_logs\2022-05-07T17_10_51_674Z-debug-0.log
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
